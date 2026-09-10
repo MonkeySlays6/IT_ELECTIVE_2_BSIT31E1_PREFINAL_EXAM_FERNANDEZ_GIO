f32e71aef9795660d0c11f47006ba8d24daf4201
Gio Fernandez <monkeydummyacc@gmail.com>
2026-09-10 18:43:52 +0800

    Added Question 16

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -204,6 +204,19 @@ public class HomeController : Controller
                 },
                 ChosenAnswer = "B"
             },
+            new ExamQuestion
+            {
+                Number = 16,
+                Question = "Which is the best reason for having a database-level unique constraint on StudentNumber?",
+                Choices = new List<string>
+                {
+                    "A. It protects data integrity even if application-level validation is bypassed",
+                    "B. It makes Razor Views render faster",
+                    "C. It removes the need for a Controller",
+                    "D. It automatically creates a ViewModel"
+                },
+                ChosenAnswer = "A"
+            },
         };
 
         return View(questions);
