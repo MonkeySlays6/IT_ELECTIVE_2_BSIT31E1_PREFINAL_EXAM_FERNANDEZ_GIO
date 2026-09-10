bdc1426e4abddf3d72391137277ca71621de918d
Gio Fernandez <monkeydummyacc@gmail.com>
2026-09-10 18:42:07 +0800

    Added Question 15

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -191,6 +191,19 @@ public class HomeController : Controller
                 },
                 ChosenAnswer = "A"
             },
+            new ExamQuestion
+            {
+                Number = 15,
+                Question = "A school requires every student to have a unique Student Number. Which rule best represents this requirement?",
+                Choices = new List<string>
+                {
+                    "A. Student Number should always be nullable",
+                    "B. Student Number should be unique",
+                    "C. Student Number should always be the same",
+                    "D. Student Number should contain only spaces"
+                },
+                ChosenAnswer = "B"
+            },
         };
 
         return View(questions);
